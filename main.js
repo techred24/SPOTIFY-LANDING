64a4ca91f1cc9635ca655cdb2412379901df95dc
TeChRF24 <f.rafael.temich24@gmail.com>
2024-06-27 21:50:51 -0600

    Rotating arrows in items for questions when clicking

--- main.js
+++ main.js
@@ -21,13 +21,38 @@ const $detailsTags = document.querySelectorAll('.details');
 $detailsTags.forEach((detailTag) => {
     // console.log(detailTag);
     detailTag.addEventListener('click', function (event) {
-        console.log(event.target);
-        console.log(event.target.getAttribute('open'));
-        if (event.target.getAttribute('open')) {
-            const $svgElement = event.target.querySelector('.details__svg');
+        console.log('PARENT TARGET');
+        const $parentElement = event.target.parentElement;
+        const attributesNodeArray = [...$parentElement.attributes];
+        const attrs = attributesNodeArray.reduce((attrs, attribute) => {
+            attrs[attribute.name] = attribute.value;
+            return attrs;
+        }, {});
+        console.log('atributos:', attrs);
+        // console.log(event.target);
+        console.log(event.target.parentElement);
+        // console.log(event.target.parentElement.nodeName);
+        console.log('open:', $parentElement.getAttribute('open'));
+        // console.log(event.target.getAttribute('open'));
+        const $svgElement = event.target.querySelector('.details__svg');
+
+        console.log('OPEN IN ATTRS');
+        console.log('open' in attrs);
+        if ($parentElement.getAttribute('open') == null) {
+        // if ('open' in attrs) {
             $svgElement.classList.add('rotate-svg');
-            console.log('here');
+            // console.log('here');
+            return;
             // $svgElement.style.transform = 'rotate(360deg)';
         }
+        $svgElement.classList.remove('rotate-svg');
     })
-})
+});
+
+// const node = document.querySelector('div')
+// const attributeNodeArray = [...node.attributes];
+// const attrs = attributeNodeArray.reduce((attrs, attribute) => {
+//   attrs[attribute.name] = attribute.value;
+//   return attrs;
+// }, {});
+// console.log(attrs)
